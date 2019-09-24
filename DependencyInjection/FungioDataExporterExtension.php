1937f6977f30b61f5baa54d56574e9360fbce60b
Pierrick Aubin <pierrick.aubin@siqual.fr>
2019-09-24 16:59:28 +0200

    Rename .yml file to .yaml

--- DependencyInjection/FungioDataExporterExtension.php
+++ DependencyInjection/FungioDataExporterExtension.php
@@ -24,6 +24,6 @@ class FungioDataExporterExtension extends Extension
         $config        = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
